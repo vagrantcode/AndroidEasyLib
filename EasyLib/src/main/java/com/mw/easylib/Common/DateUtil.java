803a2c3e71950b771d7ac3d1c2e85cb86058650c
vagrantcode <1129739140@qq.com>
2020-11-24 15:47:22 +0800

    添加时间计算工具

---
+++ EasyLib/src/main/java/com/mw/easylib/Common/DateUtil.java
@@ -0,0 +1,56 @@
+package com.mw.easylib.Common;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+    public static Date convertToDate(String strDate) throws Exception {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        return df.parse(strDate);
+    }
+
+    public static Date convertToDate(String strDate, String format) throws Exception {
+        DateFormat df = new SimpleDateFormat(format);
+        return df.parse(strDate);
+    }
+
+    public static String convertToString(Date date) {
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        return df.format(date);
+    }
+
+    public static String convertToString(Date date, String format) {
+        DateFormat df = new SimpleDateFormat(format);
+        return df.format(date);
+    }
+    public static long difTime(Date startDate,Date endDate) throws Exception{
+        long d1=startDate.getTime();
+        long d2=endDate.getTime();
+        return d1-d2;
+    }
+
+    public enum DateStringFormat {
+        DateHorizontal("yyyy-MM-dd"), DateTimeHorizontal("yyyy-MM-dd HH:mm:ss");
+        private String format = "yyyy-MM-dd";
+
+        DateStringFormat(String format) {
+            this.format = format;
+        }
+
+        public static DateStringFormat getValueOf(String val) {
+            switch (val) {
+                case "yyyy-MM-dd":
+                    return DateHorizontal;
+                case "yyyy-MM-dd HH:mm:ss":
+                    return DateTimeHorizontal;
+                default:
+                    return null;
+            }
+        }
+
+        public String value() {
+            return this.format;
+        }
+    }
+}
--- app/src/main/java/com/mw/androideasylib/MainActivity.java
+++ app/src/main/java/com/mw/androideasylib/MainActivity.java
@@ -3,6 +3,9 @@ package com.mw.androideasylib;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.mw.easylib.Common.DateUtil;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+         long res=   DateUtil.difTime(DateUtil.convertToDate("2020-09-01 13:00:00",DateUtil.DateStringFormat.DateTimeHorizontal.value()),DateUtil.convertToDate("2020-09-01 13:00:01",DateUtil.DateStringFormat.DateTimeHorizontal.value()));
+            Log.i("sdfasdfa",res+"");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
